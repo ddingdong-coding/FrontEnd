dc7d42b18f61eeb87e61c4f48de538afe59291f0
ellieheo <sunheeheo10@gmail.com>
2022-08-24 01:23:34 +0900

    feat: link copied

--- LXI/JS/story.js
+++ LXI/JS/story.js
@@ -4,16 +4,13 @@ function tem() {
     fetch("https://picsum.photos/v2/list")
         .then(res => res.json()).then((data) => {
             for (let i = 0; i < 7; i++) {
-                let user = data[i]['author']
-                if (user.length > 9) {
-                    user = user.substr(0, 8) + "..."
-                }
                 let img = data[i]['download_url']
-                let temp_html = `<div class= "storyImg"><img src="${img}">${user}</div>`
+                let temp_html = `<div class= "storyImg"><img src="${img}"></div>`
                 $('#story').append(temp_html);
 
             }
-            return document.querySelectorAll(".storyImg");
+            let re = document.querySelectorAll(".storyImg");
+            return re
 
         })
 
@@ -36,5 +33,3 @@ function renderStory() {
         })
 
 }
-
-console.log(tem())
