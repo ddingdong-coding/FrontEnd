b67627365d7456db30be708fbdbf6004fed88c8b
ellieheo <sunheeheo10@gmail.com>
2022-08-24 13:26:40 +0900

    feat: story slides

--- LXI/JS/story.js
+++ LXI/JS/story.js
@@ -3,7 +3,7 @@
 function renderStory() {
     fetch("https://picsum.photos/v2/list")
         .then(res => res.json()).then((data) => {
-            for (let i = 0; i < 9; i++) {
+            for (let i = 0; i < 10; i++) {
                 let user = data[i]['author']
                 if (user.length > 9) {
                     user = user.substr(0, 8) + "..."
--- LXI/JS/storySlide.js
+++ LXI/JS/storySlide.js
@@ -4,14 +4,14 @@ const prevBtn = document.querySelector(".slide_prev_button");
 const nextBtn = document.querySelector(".slide_next_button");
 
 (function addEvent(){
-  prevBtn.addEventListener('click', translateContainer.bind(this, 2));
-  nextBtn.addEventListener('click', translateContainer.bind(this, -2));
+  prevBtn.addEventListener('click', translateContainer.bind(this, 1));
+  nextBtn.addEventListener('click', translateContainer.bind(this, -1));
 })();
 
 function translateContainer(direction){
   const selectedBtn = (direction === 1) ? 'slide_prev_button' : 'slide_next_button';
-  container.style.transitionDuration = '500ms';
-  container.style.transform = `translateX(${direction * (100 / 9)}%)`;
+  container.style.transitionDuration = '900ms';
+  container.style.transform = `translateX(${direction * (100 / 10)}%)`;
   container.ontransitionend = () => reorganizeEl(selectedBtn);
 }
 
