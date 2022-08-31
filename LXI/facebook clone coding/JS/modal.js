a2580a86b26d4d72cdb39f1ae6285427f2cfc2bb
ellieheo <sunheeheo10@gmail.com>
2022-08-31 22:12:01 +0900

    feat:inputBox

--- LXI/facebook clone coding/JS/modal.js
+++ LXI/facebook clone coding/JS/modal.js
@@ -1,16 +1,10 @@
-$(function () {
-    $(".bi-messenger").click(function () {
-        $(".modal").fadeIn();
-    })
-    $("#main,#advice,#currentWeather").click(function () {
-        $("#modal").fadeOut();
-    })
-})
-$(function () {
-    $(".bi-bell").click(function () {
-        $(".notification").fadeIn();
-    })
-    $("#main,#advice,#currentWeather").click(function () {
-        $("#modal").fadeOut();
-    })
-})
+function openModal(modalname){
+    document.get
+    // $("#modal").fadeIn(300);
+    $("."+modalname).fadeIn(300);
+  }
+  
+  $(".main, .wrap, .gnb, post, body").on('click',function(){
+    $("#modal").fadeOut(300);
+    $(".modal-con").fadeOut(300);
+  });
