e71ff7457032a692617715c9cda25e25fc89c391
ellieheo <sunheeheo10@gmail.com>
2022-08-31 19:23:44 +0900

    feat:taps

--- LXI/facebook clone coding/JS/logIn.js
+++ LXI/facebook clone coding/JS/logIn.js
@@ -1,13 +1,15 @@
 function checkEmailandPassword() {
     let email = $("#email").val();
     let password = $("#password").val();
-    if (!email.includes('@')) {
-        alert("Please check your email again")
-    } else {
-        if (!(password == "facebook")) {
-            alert("Please check your password")
-        } else {
+    if (email.includes('@')==true) {
+        if ((password == "facebook")==true) {
+            alert("Login success")
             window.location.href = "main.html";
+        } else {
+            alert("Please check your password")
         }
+    } else {
+        alert("Please check your email again")
     }
 }
+
