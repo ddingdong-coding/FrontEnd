f8fe0c744d52237b4fa2948128810c98f770766c
ellieheo <sunheeheo10@gmail.com>
2022-09-02 22:34:07 +0900

    feat

--- mongo.py
+++ mongo.py
@@ -1,6 +1,6 @@
 import pymongo 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://ellie:mshee791008!@cluster0.xhjvgcr.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://ellie:비밀번호@cluster0.xhjvgcr.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 doc = {
     'name':'bob',
@@ -8,3 +8,9 @@ doc = {
 }
 
 db.users.insert_one(doc)
+
+db.users.insert_one({'name':'bobby','age':21})
+db.users.insert_one({'name':'kay','age':27})
+db.users.insert_one({'name':'john','age':30})
+
+same_ages = list(db.users.find({},{'_id':False}))
