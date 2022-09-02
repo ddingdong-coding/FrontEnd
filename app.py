4c4e5c8827a189b2330f3582f0ab1cf29bb06b54
ellieheo <sunheeheo10@gmail.com>
2022-09-03 00:25:52 +0900

    feat:db

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+from pymongo import MongoClient
+client = MongoClient('dburl')
+db = client.facebook
+
+from datetime import datetime
 
 @app.route('/')
 def home():
@@ -10,14 +19,20 @@ def home():
 def main():
     return render_template('main.html')
 
-app.route('/main',methods=['POST'])
-def upload_modal():
+@app.route('/main',methods=['POST'])
+def posting():
     content = request.form['content']
     print(content)
+    today = datetime.now()
     doc ={
-        content:content
+        'content':content,
+        'today':today
     }
-    # db
-    # db.articles.insert_one(doc)
+    db.posting.insert_one(doc)
+        
+    return jsonify(result={"status": 200})
+
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/js/post.js
+++ static/js/post.js
@@ -2,12 +2,12 @@ function posting(){
     let content =$("#post").val()
     const formData = new FormData();
     formData.append('content',content)
-    fetch('http://127.0.0.1:5000/main', {
+    fetch('/main', {
         method: 'POST',
-        cache: 'no-cache',
         body: formData,
       }).then((res) => {
         if(res.status ===200 || res.status ===201) {
+          console.log("성공 했니?")
           res.json().then(json => console.log(json));
         }else {
           console.error(res.statusText);
