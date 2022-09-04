f2aa35221705c31e60c52f055ac9227aa319530e
ellieheo <sunheeheo10@gmail.com>
2022-09-05 01:39:23 +0900

    file update

--- app.py
+++ app.py
@@ -71,8 +71,3 @@ def objectIdDecoder(list):
 
 if __name__ == '__main__':
     app.run('0.0.0.0',os.environ['PORT'], debug=True)
-
-# if __name__ == '__main__':
-#     app.run('0.0.0.0', port=8000, debug=True)
-# if __name__ == '__main__':
-#     app.run('0.0.0.0',debug=True)
